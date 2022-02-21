c247aa33c24a330003d96d6b0582169f77400b18
pedram soufi <pedram.soufi@part.local>
2022-02-21 13:14:50 +0330

    feat: develop

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h3>{{ msg }}</h3>
+    <div class="theme-handling">
+      <button @click="changeTheme">change theme</button>
+    </div>
   </div>
 </template>
 
@@ -10,24 +13,34 @@ export default {
   props: {
     msg: String,
   },
+  methods: {
+    changeTheme() {
+      const root = document.querySelector(':root');
+      const theme = root.getAttribute('theme')
+      root.setAttribute('theme' , theme == 'ligth' ? 'dark' : 'ligth')
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/sass/utilities/typography.scss";
+@import "@/assets/sass/abstracts";
+
 h3 {
   @include typography-H3;
   margin: 40px 0 0;
+  color: var(--text-primary)
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.theme-handling {
+  margin-top: 50px;
+  > button {
+    cursor: pointer;
+    @include typography-bold;
+    padding: 10px;
+    border-radius: $border-radius;
+    background-color: var(--clr-primary);
+    color: var(--text-primary)
+  }
 }
 </style>
